c7a0d9dc845200e18007f155df2cb2d636f84a09
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-03 09:41:22 -0600

    Section 4, working on warnings and hints as dynamic and flexible component

---
+++ Section4/react-ts-basics/src/components/CourseGoalList.tsx
@@ -0,0 +1,36 @@
+import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
+import { CourseGoal as CourseGoalType } from "../App";
+import { ReactNode } from "react";
+
+type CourseGoalListProps = {
+    goals: CourseGoalType[];
+    onDeleteGoal: (id: number) => void;
+}
+
+export default function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProps) {
+  if (goals.length === 0){
+    return <InfoBox mode="hint">
+      You have no goals yet
+    </InfoBox>
+  }  
+
+let warningBox: ReactNode;
+if(goals.length >= 4){
+  warningBox = <InfoBox mode="warning"> Too much goals </InfoBox>
+}
+
+    return (
+    <>
+    {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id ={ goal.id}title={goal.title} onDelete = {onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>)
+}
