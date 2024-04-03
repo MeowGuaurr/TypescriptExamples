91462de0456877163fac5cc1cf02a5ffe8ec85c4
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-03 15:37:51 -0600

    working with Type Predicates

--- Section4/ADV-Components/src/App.tsx
+++ Section4/ADV-Components/src/App.tsx
@@ -1,10 +1,13 @@
 //import Input from "./components/Input.tsx";
-import Button from "/components/Button.tsx";
+import Button from "./components/Button.tsx";
 
 function App(){
   return <main>
     <p>
-     h1
+     <Button>A button</Button>
+    </p>
+    <p>
+      <Button href='https://youtube.com'>A link</Button>
     </p>
   </main>
 }
--- Section4/ADV-Components/src/components/Button.tsx
+++ Section4/ADV-Components/src/components/Button.tsx
@@ -1,18 +1,16 @@
 import { type ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-    el: 'button';
-} & ComponentPropsWithoutRef<'button'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'>;
 
-type AnchorProps = {
-    el: 'anchor';
-} & ComponentPropsWithoutRef<'a'>;
+type AnchorProps = ComponentPropsWithoutRef<'a'>;
 
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+    return 'href' in props;
+}
 export default function Button(props: ButtonProps | AnchorProps) {
-    //const {el, ...otherProps} = props;
-    if (props.el === 'anchor'){
-        return <a {...props}></a>
+    if (isAnchorProps(props)) {
+        return <a className="button" {...props}></a>
     }
     
-    return <button {...props}></button>
+    return <button className="button" {...props}></button>
 }
