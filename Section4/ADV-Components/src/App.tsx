a3c7e84d6c5377776211c530467465b7eda993e7
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-03 13:24:21 -0600

    Section 4 advanced components, new project

---
+++ Section4/ADV-Components/src/App.tsx
@@ -0,0 +1,11 @@
+//import Input from "./components/Input.tsx";
+import Button from "/components/Button.tsx";
+
+function App(){
+  return <main>
+    <p>
+     h1
+    </p>
+  </main>
+}
+export default App;
