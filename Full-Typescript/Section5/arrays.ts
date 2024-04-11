5b8fa82509c27ff815b95b3733a5da341c24a3eb
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-11 10:30:40 -0600

    working with arrays and tuples

---
+++ Full-Typescript/Section5/arrays.ts
@@ -0,0 +1,23 @@
+const carMakers = ['ford','toyota','chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incompatible values with the array type
+// carMakers.push(100);
+carMakers.push('suzuki');
+carMakers.map((car: string) : string => {
+    return car.toUpperCase();
+})
+
+const importantDates: (Date | string)[] = [new Date(), '2015-08-09', 'mothers Day'];
+importantDates.push('2015-01-01');
+importantDates.push(new Date());
