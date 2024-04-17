7e7380bcbd19a418df5af20bb610ae8d7adf697f
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-17 16:53:59 -0600

    set algorithm to sort linked list and work with abstract classes

--- Full-Typescript/sort/src/index.ts
+++ Full-Typescript/sort/src/index.ts
@@ -1,7 +1,19 @@
 import { Sorter } from "./Sorter";
+import { LinkedList } from "./LinkedList";
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection  } from "./NumbersCollection";
 
 const numbersCollection = new NumbersCollection([100, 3, -5, 0])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('AjcdCDsfrJ')
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const ListCollection = new LinkedList()
+ListCollection.add(500);
+ListCollection.add(-100);
+ListCollection.add(67);
+ListCollection.sort();
+ListCollection.print();
