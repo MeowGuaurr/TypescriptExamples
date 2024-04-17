8a427e6003b3590b63ac4e8712ce86075871a3f9
MeowGuaurr <Reynitamercedes@gmail.com>
2024-04-17 10:12:28 -0600

    working with a non eficient approach

--- Full-Typescript/sort/src/index.ts
+++ Full-Typescript/sort/src/index.ts
@@ -1,15 +1,26 @@
 class Sorter {
-    constructor(public collection: number[]){};
+    constructor(public collection: number[] | string){};
 
-    sort(): void{
+    sort(): void{ 
         const { length } = this.collection;
         for (let i = 0; i < length; i++){
             for (let j = 0; j < length - i - 1; j++){
-                if(this.collection[j] > this.collection[j+1]){
+                
+            // approach for an array of numbers
+            if(this.collection instanceof Array) 
+            // using type guard to checkk type array in collection to manipulate collection as an array of numbers
+                {
+                    if(this.collection[j] > this.collection[j+1]){
                     const temp = this.collection[j];
                     this.collection[j] = this.collection[j+1];
                     this.collection[j+1] = temp;
-                } 
+                 } 
+                }
+
+            // approach for string
+            if(typeof this.collection === 'string') {
+                
+            }
             }
         }
     }
